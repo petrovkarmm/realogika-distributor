601101c867ba7918b1ee4ddf3e177052185bbda9
petrovleonid <petrovleonid1999@mail.ru>
2024-06-04 09:00:30 +0300

    ref program KBS

---
+++ telegram_bot/routers/ref_code_no_roles/keyboards.py
@@ -0,0 +1,15 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+
+
+def ref_program_menu_keyboards():
+    keyboard_builder = ReplyKeyboardBuilder()
+    keyboard_builder.button(text='Моя структура')
+    keyboard_builder.button(text='Баланс')
+    keyboard_builder.button(text='Моя реф. ссылка')
+
+    keyboard_builder.adjust(1)
+
+    return keyboard_builder.as_markup(
+        resize_keyboard=True,
+        one_time_keyboard=False
+    )
