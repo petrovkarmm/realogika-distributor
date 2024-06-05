e57c5ed1e9a02fd40eca7ee96a4cd989ed669f08
Petrov Leonid <petrovleonid1999@mail.ru>
2024-06-05 18:33:34 +0300

    ref code kb with balance

--- telegram_bot/routers/start_command/keyboards.py
+++ telegram_bot/routers/start_command/keyboards.py
@@ -12,3 +12,17 @@ def ref_code_no_roles_keyboard():
         resize_keyboard=True,
         one_time_keyboard=False
     )
+
+
+def ref_code_keyboard():
+    keyboard_builder = ReplyKeyboardBuilder()
+    keyboard_builder.button(text='Магазин')
+    keyboard_builder.button(text='Реф. программа')
+    keyboard_builder.button(text='Баланс')
+
+    keyboard_builder.adjust(1)
+
+    return keyboard_builder.as_markup(
+        resize_keyboard=True,
+        one_time_keyboard=False
+    )
