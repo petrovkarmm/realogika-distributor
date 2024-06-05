51718aa64f4dd9c721ab8a92ca883403db413f18
Petrov Leonid <petrovleonid1999@mail.ru>
2024-06-05 18:33:57 +0300

    including dialog into router

--- telegram_bot/routers/ref_program/balance_dialog/balance_dialog_router.py
+++ telegram_bot/routers/ref_program/balance_dialog/balance_dialog_router.py
@@ -1,5 +1,7 @@
 from aiogram import Router
 
+from telegram_bot.routers.ref_program.balance_dialog.balance_dialog import balance_dialog
+
 balance_dialog_router = Router()
 
 balance_dialog_router.include_router(
