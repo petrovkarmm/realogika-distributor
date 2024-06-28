20d3e38a5996c96442edfd3c8e5457ade55ca5a7
petrovleonid <petrovleonid1999@mail.ru>
2024-06-28 08:47:52 +0300

    tested middlewares for message and callback query (only prints)

---
+++ telegram_bot/middlewares/user_status_checker_middleware.py
@@ -0,0 +1,36 @@
+import logging
+from typing import Callable, Dict, Any, Awaitable
+from aiogram import BaseMiddleware
+from aiogram.enums import ChatType
+from aiogram.types import TelegramObject
+from aiogram_dialog import DialogManager
+
+
+class UserStatusCheckMessage(BaseMiddleware):
+    async def __call__(
+            self,
+            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+            event: TelegramObject,
+            data: Dict[str, Any],
+    ) -> Any:
+        # Запрос на получение инстанса юзерa
+        print('entering into message MW')
+        print(data)
+        print(event)
+        result = await handler(event, data)
+        return result
+
+
+class UserStatusCheckCallback(BaseMiddleware):
+    async def __call__(
+            self,
+            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+            event: TelegramObject,
+            data: Dict[str, Any],
+    ) -> Any:
+        # Запрос на получение инстанса юзера
+        print('entering into callback MW')
+        print(data)
+        print(event)
+        result = await handler(event, data)
+        return result
