5c929c81b994ff5f97ddc9e44fd5f7e2bc4d29c0
petrovleonid <petrovleonid1999@mail.ru>
2024-07-03 19:43:56 +0300

    adding buy shop dialog state

--- telegram_bot/routers/global_utils/shop_dialog/shop_dialog_states.py
+++ telegram_bot/routers/global_utils/shop_dialog/shop_dialog_states.py
@@ -4,3 +4,4 @@ from aiogram.fsm.state import StatesGroup, State
 class ShopDialog(StatesGroup):
     shop_dialog_menu = State()
     shop_item_detail = State()
+    shop_item_buy_accepting = State()
