d1fcbc06067267a96c4c2d27b43f613a4ec44dee
petrovleonid <petrovleonid1999@mail.ru>
2024-07-15 20:58:39 +0300

    adding object intoi dialog

--- telegram_bot/routers/global_utils/shop_dialog/shop_dialog.py
+++ telegram_bot/routers/global_utils/shop_dialog/shop_dialog.py
@@ -9,13 +9,10 @@ from aiogram_dialog.widgets.kbd import Button, ScrollingGroup, Column, Select, B
 from aiogram_dialog.widgets.text import Const, Format
 
 from telegram_bot.routers.global_utils.keyboards import close_invoice
-from telegram_bot.routers.global_utils.shop_dialog.shop_dialog_fetchers import get_all_shop_items
+from telegram_bot.routers.global_utils.shop_dialog.shop_dialog_fetchers import get_all_items_from_shop, \
+    get_item_from_shop
 from telegram_bot.routers.global_utils.shop_dialog.shop_dialog_states import ShopDialog
 from telegram_bot.routers.global_utils.shop_dialog.shop_items_dataclass import ShopItem, SHOP_KEY
-from telegram_bot.routers.ref_program.balance_dialog.balance_dataclass import BalanceMovement, BALANCE_KEY
-from telegram_bot.routers.ref_program.balance_dialog.balance_dialog_states import BalanceDialog
-from data_for_tests import test_user_balance_movement_data, \
-    test_full_balance_movement_info, shop_main_page_data_test, test_shop_item_details
 from telegram_bot.routers.start_command.keyboards import ref_code_keyboard
 
 
@@ -46,9 +43,8 @@ async def send_invoice_click(
 
     current_chat_id = callback_query.message.chat.id
 
-    current_shop_item_name = dialog_manager.dialog_data['name']
+    current_shop_item_name = dialog_manager.dialog_data['title']
     current_shop_item_description = dialog_manager.dialog_data['description']
-    current_shop_item_count = dialog_manager.dialog_data['count']
     current_shop_item_price = dialog_manager.dialog_data['price'] * 100
 
     prices = [
@@ -137,28 +133,22 @@ async def on_shop_item_selected(
         dialog_manager: DialogManager,
         shop_item_id: int,
 ):
-    shop_item_detail_info = test_shop_item_details[int(shop_item_id)]
+    shop_item_detail_info = await get_item_from_shop(shop_item_id)
 
-    item_count = shop_item_detail_info['count']
-    if item_count <= 0:
-        await callback.answer(
-            text='Товар отсутствует.'
-        )
-    else:
-        dialog_manager.dialog_data['name'] = shop_item_detail_info['name']
-        dialog_manager.dialog_data['count'] = item_count
-        dialog_manager.dialog_data['price'] = shop_item_detail_info['price']
-        dialog_manager.dialog_data['description'] = shop_item_detail_info['description']
+    dialog_manager.dialog_data['title'] = shop_item_detail_info['title']
+    dialog_manager.dialog_data['price'] = shop_item_detail_info['offers'][0]['price']
+    dialog_manager.dialog_data['action'] = shop_item_detail_info['action']['name']
+    dialog_manager.dialog_data['description'] = shop_item_detail_info['offers'][0]['description']
 
-        await dialog_manager.switch_to(
-            ShopDialog.shop_item_detail
-        )
+    await dialog_manager.switch_to(
+        ShopDialog.shop_item_detail
+    )
 
 
 async def shop_items_getter(**_kwargs):
     dialog_manager = _kwargs['dialog_manager']
 
-    shop_items_object = await get_all_shop_items()
+    shop_items_object = await get_all_items_from_shop()
 
     pprint(shop_items_object)
 
@@ -208,19 +198,14 @@ shop_menu_window = Window(
     state=ShopDialog.shop_dialog_menu
 )
 
-# dialog_manager.dialog_data['name'] = shop_item_detail_info['name']
-# dialog_manager.dialog_data['count'] = shop_item_detail_info['count']
-# dialog_manager.dialog_data['price'] = shop_item_detail_info['price']
-# dialog_manager.dialog_data['description'] = shop_item_detail_info['description']
-
 shop_item_detail_window = Window(
     Format(
-        "Название товара: {dialog_data[name]}."
+        "Название товара: {dialog_data[title]}."
         "\nЦена: {dialog_data[price]} руб."
         "\nОписание: {dialog_data[description]}",
     ),
     Button(
-        text=Const('Перейти к покупке'), id='go_to_buy_item', on_click=go_to_item_buy_accepting
+        text=Format('{dialog_data[action]}'), id='go_to_buy_item', on_click=go_to_item_buy_accepting
     ),
     Row(
         Button(
@@ -237,12 +222,11 @@ shop_item_detail_window = Window(
 shop_item_buy_accepting_window = Window(
     Format(
         "Пожалуйста, подтвердите покупку.\n\n"
-        "{dialog_data[name]}\n"
-        "Количество: {dialog_data[count]}\n"
-        "Цена: {dialog_data[price]}"
+        "{dialog_data[title]}\n"
+        "Цена: {dialog_data[price]} рублей"
     ),
     Button(
-        text=Const("Купить"), id='buy_item', on_click=send_invoice_click
+        text=Const("Подтверждаю"), id='buy_item', on_click=send_invoice_click
     ),
     Row(
         Button(
