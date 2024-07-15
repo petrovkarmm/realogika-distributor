84343244fc734a5ffdc7d0e441b90873de7f63ea
petrovleonid <petrovleonid1999@mail.ru>
2024-07-15 20:59:09 +0300

    adding detail item info fetcher

--- telegram_bot/routers/global_utils/shop_dialog/shop_dialog_fetchers.py
+++ telegram_bot/routers/global_utils/shop_dialog/shop_dialog_fetchers.py
@@ -3,8 +3,15 @@ import aiohttp
 from telegram_bot.settings import api_url
 
 
-async def get_all_shop_items():
+async def get_all_items_from_shop():
     async with aiohttp.ClientSession() as session:
         url = f'{api_url}/items'
+        async with session.get(url) as response:
+            return await response.json()
+
+
+async def get_item_from_shop(item_id):
+    async with aiohttp.ClientSession() as session:
+        url = f'{api_url}/item/{item_id}'
         async with session.get(url) as response:
             return await response.json()
