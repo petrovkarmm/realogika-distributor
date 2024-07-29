dffb2d13fd84a82721cc533dd35921fd1ad7f61b
petrovleonid <petrovleonid1999@mail.ru>
2024-07-29 20:58:33 +0300

    adding post_payment, patch_payment functions for future

--- telegram_bot/routers/global_utils/shop_dialog/shop_dialog_fetchers.py
+++ telegram_bot/routers/global_utils/shop_dialog/shop_dialog_fetchers.py
@@ -15,3 +15,17 @@ async def get_item_from_shop(item_id):
         url = f'{api_url}/item/{item_id}'
         async with session.get(url) as response:
             return await response.json()
+
+
+async def post_create_payment(payment_data: dict):
+    async with aiohttp.ClientSession() as session:
+        url = f'{api_url}/payment'
+        async with session.post(url=url, data=payment_data) as response:
+            return await response.json()
+
+
+async def patch_change_payment_status(payment_id: str):
+    async with aiohttp.ClientSession() as session:
+        url = f'{api_url}/payment/{payment_id}'
+        async with session.post(url=url) as response:
+            return await response.json()
