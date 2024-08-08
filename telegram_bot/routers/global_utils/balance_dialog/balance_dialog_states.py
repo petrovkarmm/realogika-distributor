b56842fe3dd17864270636416aa6793f462ee17c
Petrov Leonid <petrovleonid1999@mail.ru>
2024-08-08 21:09:55 +0300

    balance dialog state

--- telegram_bot/routers/global_utils/balance_dialog/balance_dialog_states.py
+++ telegram_bot/routers/global_utils/balance_dialog/balance_dialog_states.py
@@ -3,5 +3,8 @@ from aiogram.fsm.state import StatesGroup, State
 
 class BalanceDialog(StatesGroup):
     balance_dialog_menu = State()
-    balance_movement_detail = State()
 
+    user_rewards = State()
+    reward_detail = State()
+
+    money_movement = State()
