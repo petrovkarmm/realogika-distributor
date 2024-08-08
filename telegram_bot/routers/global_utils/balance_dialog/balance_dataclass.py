d14fd56bf3a8fae1fcd82b2906c55ec9823ef53f
Petrov Leonid <petrovleonid1999@mail.ru>
2024-08-08 21:10:24 +0300

    new reward static method

--- telegram_bot/routers/global_utils/balance_dialog/balance_dataclass.py
+++ telegram_bot/routers/global_utils/balance_dialog/balance_dataclass.py
@@ -1,19 +1,14 @@
 from dataclasses import dataclass
 from datetime import datetime
 
-BALANCE_KEY = "balance"
+REWARD_KEY = "reward"
 
 
 @dataclass
-class BalanceMovement:
+class RewardMovement:
     id: int
     amount: str
 
     @staticmethod
-    def format_amount(amount: int or float, is_accrual: bool):
-        """
-        Отображения плюса и минуса в зависимости от типа передвижения
-        """
-        if is_accrual:
-            return f'Пополнение {amount}'
-        return f'Списание {amount}'
+    def format_amount(amount: int or float):
+        return f'Вознаграждение +{amount}'
