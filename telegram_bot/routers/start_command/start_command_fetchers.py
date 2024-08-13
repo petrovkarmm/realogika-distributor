47f8f12c029b44900b1d751029e7e90e4edd7a40
petrovleonid <petrovleonid1999@mail.ru>
2024-08-13 22:55:39 +0300

    adding json with user_create into patch fetcher

--- telegram_bot/routers/start_command/start_command_fetchers.py
+++ telegram_bot/routers/start_command/start_command_fetchers.py
@@ -3,12 +3,12 @@ import aiohttp
 from telegram_bot.settings import api_url
 
 
-async def patch_user_promocode(promocode_name: str, telegram_user_id: int):
+async def patch_user_promocode(promocode_name: str, telegram_user_id: int, sponsored_user_data: dict):
     async with aiohttp.ClientSession() as session:
         url = (f'{api_url}/users/partner/link?'
                f'user_external_id={telegram_user_id}&'
                f'promocode={promocode_name}')
-        async with session.patch(url) as response:
+        async with session.patch(url, json=sponsored_user_data) as response:
             status_code = response.status
             promocode_response = await response.json()
             return status_code, promocode_response
