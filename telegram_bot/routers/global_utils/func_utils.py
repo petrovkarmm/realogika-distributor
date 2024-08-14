c0063c98be8e92c97001219d348e2901c6dad839
Petrov Leonid <petrovleonid1999@mail.ru>
2024-08-14 15:12:57 +0300

    adding qr code generation

--- telegram_bot/routers/global_utils/func_utils.py
+++ telegram_bot/routers/global_utils/func_utils.py
@@ -1,5 +1,11 @@
 import uuid
 
+import qrcode
+from aiofiles import os
+from aiogram.types import FSInputFile, Message
+
+from telegram_bot.settings import BOT_BASE_DIR
+
 
 async def uuid_generation():
     return str(uuid.uuid1())
@@ -8,3 +14,33 @@ async def uuid_generation():
 async def split_name_id_promocode(input_string):
     promocode_id, _, promocode_name = input_string.partition('_')
     return promocode_id, promocode_name
+
+
+async def send_ref_link_with_qr(ref_link: str,
+                           message: Message):
+    img = qrcode.make(ref_link)
+
+    file_uuid_name = uuid.uuid4()
+
+    qr_path = f"{BOT_BASE_DIR}\\telegram_bot\\routers\\global_utils\\qr_codes\\{file_uuid_name}.png"
+
+    img.save(qr_path)
+
+    photo = FSInputFile(qr_path)
+
+    await message.answer(
+        text='Ваша реферальная ссылка: \n'
+             f'{ref_link}'
+    )
+
+    try:
+        await message.answer_photo(
+            photo=photo
+        )
+    except Exception as e:
+        print(e)
+
+    try:
+        await os.remove(qr_path)
+    except Exception as e:
+        print(e)
