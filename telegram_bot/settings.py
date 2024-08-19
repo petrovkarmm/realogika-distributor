690032c4fccac56171e0a5472326229feea5dad7
petrovleonid <petrovleonid1999@mail.ru>
2024-08-19 19:55:10 +0300

    changing path + adding headers for future secrets

--- telegram_bot/settings.py
+++ telegram_bot/settings.py
@@ -1,6 +1,11 @@
 import os
 
-api_url = 'http://45.128.207.173:8070/api'
+# api_url = 'http://45.128.207.173:8070/api'
+api_url = 'https://5.253.62.213:8070/api'
 
+# token = os.getenv("BOT_TOKEN")
+#
+# headers = {"Authorization": f"Bearer {token}"}
 
-BOT_BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BOT_BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BOT_BASE_DIR = os.getcwd()
