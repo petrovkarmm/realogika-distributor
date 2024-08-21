d0458deffb5cae04ff5176bbd611128913d323e6
petrovleonid <petrovleonid1999@mail.ru>
2024-08-22 01:17:31 +0300

    conf for test

--- telegram_bot/settings.py
+++ telegram_bot/settings.py
@@ -1,11 +1,14 @@
 import os
 
+from dotenv import load_dotenv, find_dotenv
+
 # api_url = 'http://45.128.207.173:8070/api'
-api_url = 'https://5.253.62.213:8070/api'
+api_url = 'http://5.253.62.213:8070/api'
 
-token = os.getenv("BOT_TOKEN")
+load_dotenv(find_dotenv())
+token = os.getenv("token")
 
-headers = {"Authorization": f"Bearer {token}"}
+headers = {"Authorization": f"Bearer test"}
 
 # BOT_BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BOT_BASE_DIR = os.getcwd()
